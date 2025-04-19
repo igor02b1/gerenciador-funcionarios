2ea6e2c8ac967324609db98d614a6dac56a6f586
Igor de Lima <igor02b1@gmail.com>
2025-04-19 17:59:11 -0300

    Iniciando o layout e adicionando a lógica para conectar com a api

---
+++ gerenciador-funcionarios/src/app/Models/Funcionarios.ts
@@ -0,0 +1,11 @@
+export interface Funcionario{
+    id? : number;
+    nome: string;
+    sobrenome: string;
+    departamento: string;
+    ativo: string;
+    turn: string;
+    dataDeCriacao? : string;
+    dataDeAlteracao? : string;
+
+}
