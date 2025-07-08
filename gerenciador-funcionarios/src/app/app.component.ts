9e4dfe83b4280291bae0cdf3888e4ccf275f3b81
Igor02b1 <igor02b1@gmail.com>
2025-07-08 03:01:06 -0300

    criando rota para cadastro

--- gerenciador-funcionarios/src/app/app.component.ts
+++ gerenciador-funcionarios/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { RouterModule, Routes } from '@angular/router';
 
 
 @Component({
   selector: 'app-root',
-  imports: [HomeComponent],
+  imports: [HomeComponent, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
